8c57e96f8704deb5872a8e282b4aa38033d05883
Alvin Chou <alvinchou516@hotmail.com>
2022-03-15 14:31:18 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Header from './Header';
+
+//  import 2 pieces from the router library which will allow us to configure the routes witin this app
+import { Routes, Route } from 'react-router-dom';
+import MovieCatalogue from './MovieCatalogue';
+import MovieInfo from './MovieInfo';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {/* This app consists of 3 components: */}
+      {/* a header */}
+      <Header />
+
+      {/* now we need to define our routing configuration (this is often done within the App component) */}
+
+      {/* Step 1: use the Routes component to act as a parent container to all of the subsequently defined Routes AKA define URL paths */}
+      <Routes>
+        {/* Step 2: define the individual routes / URL paths with are available withinin your app, as well as the compoenents which are visible at those paths */}
+        {/* here is where you will define which component is visible at what path */}
+        <Route path="/" element={ <MovieCatalogue /> }/>
+
+
+        {/* the MovieInfo component should be shown when the route looks like: */}
+        {/* www.homepage.com/movieIdHere */}
+        {/* :movieI is like a url param */}
+        <Route path="/:movieId" element={ <MovieInfo /> }></Route>
+      
+      
+      </Routes>
+      {/* a Movie Catalogue */}
+        {/* this compnent is only visible on the homepage */}
+
+      {/* a Movie Details compnent (which is rendred  when the user selects a movie) */}
     </div>
   );
 }
